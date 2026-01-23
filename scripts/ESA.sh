2fc90ba0286c6d5993284df173e7ff89ab7ceca1
Angus-Eastell <sd23327@bristol.ac.uk>
2026-01-23 11:04:02 +0000

    chnages

--- scripts/ESA.sh
+++ scripts/ESA.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
 
-python main.py --anormly_ratio 1 --num_epochs 2   --batch_size 256  --mode train --dataset 3_months  --data_path dataset/ESA   --input_c 6 --output_c 6
-python main.py --anormly_ratio 1 --num_epochs 10   --batch_size 256     --mode test    --dataset 3_months   --data_path dataset/ESA     --input_c 6     --pretrained_model 20
+python main_esa.py --anormly_ratio 1 --num_epochs 1   --batch_size 256  --mode train --dataset 1_months  --data_path dataset/ESA --train_length 1_months --test_length 1_months --input_c 6 --output_c 6
+python main_esa.py --anormly_ratio 1 --num_epochs 1   --batch_size 256     --mode test    --dataset 1_months   --data_path dataset/ESA --train_length 1_months --train_length 1_months --test_length 84_months --input_c 6 --output_c 6 --pretrained_model 20
