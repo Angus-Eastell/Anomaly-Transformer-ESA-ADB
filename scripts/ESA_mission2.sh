1f77da6be3641f97cef70237d931fc3737c25273
Angus Eastell <sd23327@bristol.ac.uk>
2026-02-07 15:44:28 +0000

    scripts

---
+++ scripts/ESA_mission2.sh
@@ -0,0 +1,5 @@
+export CUDA_VISIBLE_DEVICES=0
+
+python main_esa.py --mission mission_2 --num_epochs 20   --batch_size 64  --mode train --dataset 21_months  --data_path dataset/ESA_mission2 --train_length 21_months --test_length 21_months --input_c 11 --output_c 11 --step 25 --win_size 100 --run_name mission2
+python main_esa.py --mission mission_2 --anormly_ratio 0.1  --batch_size 64     --mode test    --dataset 21_months   --data_path dataset/ESA_mission2 --train_length 21_months --test_length 21_months --input_c 11 --output_c 11 --step 1 --win_size 100 --run_name misson2 --temperature 50 
+#python main_esa.py --anormly_ratio 0.1  --batch_size 64     --mode thresh_analysis    --dataset 84_months   --data_path dataset/ESA --train_length 84_months --test_length 84_months --input_c 6 --output_c 6 --step 1 --win_size 100 --run_name thresh --pretrained_model 20 --temperature 50 --n_heads 4 --d_model 256
